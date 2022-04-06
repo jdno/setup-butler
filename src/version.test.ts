68f30624f483ec6d1c01329d731dea96df705f19
Jan David <jdno@jdno.dev>
2022-04-06 14:19:59 +0200

    Set up unit tests (#9)
    Jest has been set up as the test runner for this project, and a first
    proof-of-concept test has been written. The tests are run as part of the
    GitHub Actions continuous integration.

---
+++ src/version.test.ts
@@ -0,0 +1,23 @@
+import * as core from "@actions/core";
+
+import { getVersion } from "./version";
+
+afterEach(() => {
+  jest.resetAllMocks();
+  jest.clearAllMocks();
+});
+
+test("returns version when set", async () => {
+  const inputSpy = jest.spyOn(core, "getInput");
+  inputSpy.mockImplementation(() => "v1");
+
+  const version = await getVersion();
+
+  expect(version).toEqual("v1");
+});
+
+test("returns latest when not set", async () => {
+  const version = await getVersion();
+
+  expect(version).toEqual("latest");
+});
